5aadd7004e6ff9985528723074a69ab9504d2155
Neil Dorin <ndorin@pepperdash.com>
2025-10-03 13:44:54 -0600

    feat: Update SendFullStatus to accept an identifier
    Modified the `SendFullStatus` method to include a string parameter `id`. Updated all calls to `SendFullStatus` in `AddAction` and the event handler for `CameraAutoModeIsOnFeedback.OutputChange` to pass the `id`, allowing for more flexible status message handling.

--- src/CameraAutoModeMessenger.cs
+++ src/CameraAutoModeMessenger.cs
@@ -20,22 +20,22 @@ namespace OneBeyondAutomateVxEpi
             {
             base.RegisterActions();
           
-            AddAction("/fullStatus", (id, content) => SendFullStatus());
+            AddAction("/fullStatus", (id, content) => SendFullStatus(id));
             AddAction($"/cameraAutoModeOn", (id, context) => _cameramodedevice.CameraAutoModeOn());
             AddAction($"/cameraAutoModeOff", (id, context) => _cameramodedevice.CameraAutoModeOff());
             AddAction($"/cameraAutoModeToggle", (id, context) => _cameramodedevice.CameraAutoModeToggle());
 
-            _cameramodedevice.CameraAutoModeIsOnFeedback.OutputChange += (o, a) => SendFullStatus();
+            _cameramodedevice.CameraAutoModeIsOnFeedback.OutputChange += (o, a) => SendFullStatus(null);
 
             }
 
-        private void SendFullStatus()
+        private void SendFullStatus(string id)
             {
             var state = new IHasCameraAutoModeMessage
                 {
                 CameraAutoModeStatus = _cameramodedevice.CameraAutoModeIsOnFeedback.BoolValue
                 };
-            PostStatusMessage(state);
+            PostStatusMessage(state, id);
             }
         }
 
